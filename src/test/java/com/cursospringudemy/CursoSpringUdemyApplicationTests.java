46f433bd439dba3b99e93abe464cc119e4f16c9b
Paulo Assumpção <paulo.henriquejcn@gmail.com>
2021-02-16 19:22:35 -0300

    Projeto criado

---
+++ src/test/java/com/cursospringudemy/CursoSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospringudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
